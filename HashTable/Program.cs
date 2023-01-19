b4f417a6acb36b6666c95dfd09b14cc24ce51f13
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2023-01-19 22:33:39 +0530

    Added Find Frequency Sequence Program

--- HashTable/Program.cs
+++ HashTable/Program.cs
@@ -5,7 +5,17 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Displaying HashTable Program");
-           
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+            hash.Add("0", "To");
+            hash.Add("1", "Be");
+            hash.Add("2", "Or");
+            hash.Add("3", "Not");
+            hash.Add("4", "To");
+            hash.Add("5", "Be");
+
+            Console.WriteLine(hash.GetV("0"));
+            Console.WriteLine(hash.GetV("5"));
+            Console.ReadLine();
         }
     }
 }
